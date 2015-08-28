e5ed0344c0f0772716bf6bd84e2f37bd7ef4faa8
austinemayer <austinemayer@gmail.com>
2015-08-28 00:59:36 -0400

    Setup the environment. Input test less to make sure compiler and autoprefixer are working.

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+
+var gulp = require('gulp');
+
+var jshint = require('gulp-jshint');
+var autoprefixer = require('gulp-autoprefixer');
+var less = require('gulp-less');
+
+
+
+gulp.task('lint', function() {
+    return gulp.src('js/main.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('less', function(){
+    return gulp.src('less/style.less')
+        .pipe(less())
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(gulp.dest('css/'));
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch('scripts/*.js', ['lint-js']);
+    gulp.watch('less/*.less', ['less']);
+});
+
+
+gulp.task('default', ['watch']);
